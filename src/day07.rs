08ed65b9bfa453c4592d263e2e89f7ab0422022f
Tyler Hawkes <tyler.hawkes@gmail.com>
2021-12-07 16:05:47 -0700

    Day 7 complete

--- src/day07.rs
+++ src/day07.rs
@@ -1,4 +1,32 @@
-
 pub fn compute(input: &str) -> (usize, usize) {
-  (0, 0)
+  let positions = input
+    .split(',')
+    .map(|s| s.parse::<u16>().unwrap())
+    .collect::<Vec<_>>();
+  let (min, max) = positions
+    .iter()
+    .copied()
+    .fold((u16::MAX, u16::MIN), |l, r| (l.0.min(r), l.1.max(r)));
+  let mut min_cost_1 = usize::MAX;
+  for i in min..=max {
+    let cost = positions
+      .iter()
+      .copied()
+      .map(|x| x.max(i) as usize - x.min(i) as usize)
+      .sum::<usize>();
+    min_cost_1 = cost.min(min_cost_1);
+  }
+  let mut min_cost_2 = usize::MAX;
+  for i in min..=max {
+    let cost = positions
+      .iter()
+      .copied()
+      .map(|x| {
+        let diff = x.max(i) as usize - x.min(i) as usize;
+        (diff * diff + diff) / 2
+      })
+      .sum::<usize>();
+    min_cost_2 = cost.min(min_cost_2);
+  }
+  (min_cost_1, min_cost_2)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 // mod day03;
 // mod day04;
 // mod day05;
-mod day06;
-// mod day07;
+// mod day06;
+mod day07;
 // mod day08;
 // mod day09;
 // mod day10;
@@ -30,8 +30,8 @@ fn main() {
   // dbg!(day03::compute(include_str!("../input/day03.txt")));
   // dbg!(day04::compute(include_str!("../input/day04.txt")));
   // dbg!(day05::compute(include_str!("../input/day05.txt")));
-  dbg!(day06::compute(include_str!("../input/day06.txt")));
-  // dbg!(day07::compute(include_str!("../input/day07.txt")));
+  // dbg!(day06::compute(include_str!("../input/day06.txt")));
+  dbg!(day07::compute(include_str!("../input/day07.txt")));
   // dbg!(day08::compute(include_str!("../input/day08.txt")));
   // dbg!(day09::compute(include_str!("../input/day09.txt")));
   // dbg!(day10::compute(include_str!("../input/day10.txt")));
