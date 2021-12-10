0552da98f95e55fe8df663eb7f4936fca0e04774
Tyler Hawkes <tyler.hawkes@gmail.com>
2021-12-09 23:02:23 -0700

    Day 10 complete

--- src/day10.rs
+++ src/day10.rs
@@ -1,4 +1,91 @@
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Direction {
+  Open(Brace),
+  Close(Brace),
+}
+
+impl Direction {
+  fn opposite(self) -> Self {
+    match self {
+      Self::Open(b) => Self::Close(b),
+      Self::Close(b) => Self::Open(b),
+    }
+  }
+  fn inner(self) -> Brace {
+    match self {
+      Self::Open(b) | Self::Close(b) => b,
+    }
+  }
+}
+
+impl From<char> for Direction {
+  fn from(c: char) -> Self {
+    match c {
+      '{' => Self::Open(Brace::Curly),
+      '}' => Self::Close(Brace::Curly),
+      '(' => Self::Open(Brace::Paren),
+      ')' => Self::Close(Brace::Paren),
+      '[' => Self::Open(Brace::Square),
+      ']' => Self::Close(Brace::Square),
+      '<' => Self::Open(Brace::Angle),
+      '>' => Self::Close(Brace::Angle),
+      _ => unreachable!(),
+    }
+  }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Brace {
+  Curly,
+  Angle,
+  Square,
+  Paren,
+}
+
+impl Brace {
+  fn syntax_error_score(self) -> usize {
+    match self {
+      Self::Curly => 1197,
+      Self::Angle => 25137,
+      Self::Square => 57,
+      Self::Paren => 3,
+    }
+  }
+  fn completion_score(self) -> usize {
+    match self {
+      Self::Curly => 3,
+      Self::Angle => 4,
+      Self::Square => 2,
+      Self::Paren => 1,
+    }
+  }
+}
 
 pub fn compute(input: &str) -> (usize, usize) {
-  (0, 0)
+  let mut syntax_error_score = 0;
+  let mut incomplete = Vec::new();
+  'outer: for l in input.lines() {
+    let mut order = Vec::new();
+    for d in l.chars().map(Direction::from) {
+      match d {
+        Direction::Open(_) => order.push(d),
+        Direction::Close(b) => {
+          let last = order.pop().unwrap();
+          if last.opposite() != d {
+            // corrupted
+            syntax_error_score += b.syntax_error_score();
+            continue 'outer;
+          }
+        }
+      }
+    }
+    let completion_score = order
+      .iter()
+      .rev()
+      .copied()
+      .fold(0, |s, d| s * 5 + d.inner().completion_score());
+    incomplete.push(completion_score)
+  }
+  incomplete.sort();
+  (syntax_error_score, incomplete[incomplete.len() / 2])
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@
 // mod day06;
 // mod day07;
 // mod day08;
-mod day09;
-// mod day10;
+// mod day09;
+mod day10;
 // mod day11;
 // mod day12;
 // mod day13;
@@ -33,8 +33,8 @@ fn main() {
   // dbg!(day06::compute(include_str!("../input/day06.txt")));
   // dbg!(day07::compute(include_str!("../input/day07.txt")));
   // dbg!(day08::compute(include_str!("../input/day08.txt")));
-  dbg!(day09::compute(include_str!("../input/day09.txt")));
-  // dbg!(day10::compute(include_str!("../input/day10.txt")));
+  // dbg!(day09::compute(include_str!("../input/day09.txt")));
+  dbg!(day10::compute(include_str!("../input/day10.txt")));
   // dbg!(day11::compute(include_str!("../input/day11.txt")));
   // dbg!(day12::compute(include_str!("../input/day12.txt")));
   // dbg!(day13::compute(include_str!("../input/day13.txt")));
