c5c69c7a89d765a1d8a26af5fed0e496a779c21d
Tyler Hawkes <tyler.hawkes@gmail.com>
2021-12-12 22:25:06 -0700

    Day 13 complete

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,68 @@
+use std::collections::HashSet;
 
 pub fn compute(input: &str) -> (usize, usize) {
-  (0, 0)
+  let mut split = input.split("\n\n");
+  let dots = split.next().unwrap();
+  let mut dots = dots
+    .lines()
+    .map(|l| {
+      let mut l_split = l.split(',');
+      (
+        l_split.next().unwrap().parse::<usize>().unwrap(),
+        l_split.next().unwrap().parse::<usize>().unwrap(),
+      )
+    })
+    .collect::<HashSet<_>>();
+  let instructions = split.next().unwrap();
+  let instructions = instructions
+    .lines()
+    .map(|l| {
+      let mut l_split = l.strip_prefix("fold along ").unwrap().split('=');
+      match l_split.next().unwrap() {
+        "x" => (l_split.next().unwrap().parse::<usize>().unwrap(), 0),
+        "y" => (0, l_split.next().unwrap().parse::<usize>().unwrap()),
+        _ => unreachable!(),
+      }
+    })
+    .collect::<Vec<_>>();
+
+  dbg!(dots.len());
+  let mut first_left = 0;
+  for (x, y) in instructions {
+    let changes = match (x, y) {
+      (x, 0) => dots
+        .iter()
+        .copied()
+        .filter_map(|d| (d.0 > x).then(|| (d, (x - (d.0 - x), d.1))))
+        .collect::<Vec<_>>(),
+      (0, y) => dots
+        .iter()
+        .copied()
+        .filter_map(|d| (d.1 > y).then(|| (d, (d.0, y - (d.1 - y)))))
+        .collect::<Vec<_>>(),
+      _ => unreachable!(),
+    };
+    for (orig, new) in changes {
+      dots.remove(&orig);
+      dots.insert(new);
+    }
+    if first_left == 0 {
+      first_left = dots.len();
+    }
+  }
+  let (x_max, y_max) = dots
+    .iter()
+    .copied()
+    .fold((0, 0), |next, max| (max.0.max(next.0), max.1.max(next.1)));
+  for y in 0..=y_max {
+    for x in 0..=x_max {
+      if dots.contains(&(x, y)) {
+        print!("#");
+      } else {
+        print!(".");
+      }
+    }
+    println!()
+  }
+  (first_left, 0)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@
 // mod day08;
 // mod day09;
 // mod day10;
-mod day11;
+// mod day11;
 // mod day12;
-// mod day13;
+mod day13;
 // mod day14;
 // mod day15;
 // mod day16;
@@ -35,9 +35,9 @@ fn main() {
   // dbg!(day08::compute(include_str!("../input/day08.txt")));
   // dbg!(day09::compute(include_str!("../input/day09.txt")));
   // dbg!(day10::compute(include_str!("../input/day10.txt")));
-  dbg!(day11::compute(include_str!("../input/day11.txt")));
+  // dbg!(day11::compute(include_str!("../input/day11.txt")));
   // dbg!(day12::compute(include_str!("../input/day12.txt")));
-  // dbg!(day13::compute(include_str!("../input/day13.txt")));
+  dbg!(day13::compute(include_str!("../input/day13.txt")));
   // dbg!(day14::compute(include_str!("../input/day14.txt")));
   // dbg!(day15::compute(include_str!("../input/day15.txt")));
   // dbg!(day16::compute(include_str!("../input/day16.txt")));
